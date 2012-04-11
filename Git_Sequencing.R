15517fababbbe4b7384c41f426a4ffd46e41ec15
Aron Lindberg <aron.lindberg@case.edu>
2012-04-11 12:18:31 -0400

    Initial files for sequencing example data

---
+++ Git_Sequencing.R
@@ -0,0 +1,33 @@
+#Set the working directory
+setwd("~/Dropbox/R")
+
+# Direct output to a textfile
+sink("Sequence_Output.txt", append=FALSE, split=FALSE)
+
+#Load the TraMineR library
+library(TraMineR)
+
+#Load the dataset
+github <- read.csv(file = "GitSequences.csv", header = TRUE)
+
+#Display summary statistics
+summary(github)
+
+#Convert to compressed format
+github.comp <- seqconc(github)
+
+# Display summary
+summary(github.comp)
+
+# Create a sequence object
+github.seq<-seqdef(github)
+
+#Display summary
+summary(github.seq)
+
+#Define costs and create a distance matrix
+costs <- seqsubm(github.seq, method="TRATE")
+github.om <- seqdist(github.seq, method="OM", indel=1, sm=costs)
+
+# Display the output in rounded format
+round(github.om)
